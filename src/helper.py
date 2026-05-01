f5a0e8673b71cbdd22c3ef6c1e80a3abf58023d9
Shuvo <suvo10.info@gmail.com>
2026-05-01 14:49:29 +0600

    bhilj

--- src/helper.py
+++ src/helper.py
@@ -8,16 +8,6 @@ def load_pdf_files(data):
     loader=DirectoryLoader(data, glob="**/*.pdf", show_progress=True, loader_cls=PyPDFLoader)
     documents=loader.load()
     return documents
-## ei tuku extra chilo 
-def split_documents(documents: List[Document], chunk_size: int = 1000, chunk_overlap: int = 200):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    return text_splitter.split_documents(documents)
-
-def get_embeddings():
-    return HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
-## ei porjonto 
-
 
 
 
@@ -36,3 +26,4 @@ def download_embeddings():
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
     embeddings = HuggingFaceEmbeddings(model_name=model_name)
     return embeddings
+
