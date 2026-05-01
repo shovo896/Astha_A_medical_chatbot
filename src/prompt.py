ed0a0caac7e4f4ddce09653038aa93cc246e9450
Shuvo <suvo10.info@gmail.com>
2026-05-01 15:58:23 +0600

    iujio

--- src/prompt.py
+++ src/prompt.py
@@ -1,11 +1,14 @@
-system_prompt=(
+from langchain_core.prompts import ChatPromptTemplate
+
+
+system_prompt = (
     "You are a helpful medical assistant for answering questions about medical answers. "
     "Use the following retrieved documents to answer the question. "
     "If you don't know the answer, say you don't know. "
     "{context}"
 )
 
-prompt=ChatPromptTemplate.from_messages([
+prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
-    ("human", "{input}")
+    ("human", "{input}"),
 ])
