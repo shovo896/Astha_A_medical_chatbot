1aecf034195df43e97aa65d7d9d64ca01d04696a
Shuvo <suvo10.info@gmail.com>
2026-05-01 12:24:18 +0600

    iuhui

--- src/helper.py
+++ src/helper.py
@@ -8,10 +8,26 @@ def load_pdf_files(data):
     loader=DirectoryLoader(data, glob="**/*.pdf", show_progress=True, loader_cls=PyPDFLoader)
     documents=loader.load()
     return documents
-
+## ei tuku extra chilo 
 def split_documents(documents: List[Document], chunk_size: int = 1000, chunk_overlap: int = 200):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     return text_splitter.split_documents(documents)
 
 def get_embeddings():
     return HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+## ei porjonto 
+
+
+
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    minimal_docs : List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source", "")
+        minimal_doc = Document(
+            page_content=doc.page_content,
+            metadata={"source": src}
+        )
+        minimal_docs.append(minimal_doc)
+    return minimal_docs
